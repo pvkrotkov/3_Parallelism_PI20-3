9e295fe80f18dc9c9da599ad238dda6e8b46bb93
Никита Орайло <90414850+6Nkita9@users.noreply.github.com>
2021-11-11 02:25:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
 
-
-def element(index, A, B):
-    i, j = index
-    res = 0
-    # get a middle dimension
-    N = len(A[0]) or len(B)
-    for k in range(N):
-        res += A[i][k] * B[k][j]
-    return res
+from random import randint
+from multiprocessing import Pool
 
 
-matrix1 = [[1, 2], [3, 4]]
-matrix2 = [[2, 0], [1, 2]]
 
-print(element((1, 0), matrix1, matrix2))
+def write(a):
+    with open('multimatrix.txt', 'a') as f:
+        f.write(str(a) + " ")
+        # print(str(a))
+
+
+
+def multiplication(x, y):
+    rez = sum(i*k for i, k in zip(x, y))
+    return rez
+
+
+matrix1 = [[randint(0, 15) for i in range(3)] for i in range(3)]
+print(f'Первая матрица = {matrix1}')
+
+matrix2 = [[randint(0, 10) for i in range(3)] for i in range(3)]
+print(f'Вторая матрица = {matrix2}')
+
+
+
+# final_rez = []
+
+with Pool(4) as pool:
+    matric = pool.starmap(multiplication, [(i, k) for i in matrix1 for k in zip(*matrix2)])
+    print(matric)
+    write(matric)
