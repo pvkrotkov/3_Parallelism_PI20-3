67d4936aa00a30659300c68467405c4c9348238c
Sibilyov <90633901+Sibilyov@users.noreply.github.com>
2021-11-11 12:45:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,74 +1,52 @@
-from random import randint
 from multiprocessing import Pool
 
-
-def matrix_gen(matrix_size, max_value):
-    matrix_a = [[randint(0, max_value) for j in range(matrix_size)] for i in range(matrix_size)]
-    matrix_b = [[randint(0, max_value) for j in range(matrix_size)] for i in range(matrix_size)]
-    return matrix_a, matrix_b
-
-
-def print_matrix(matrix):
-    for i in range(len(matrix)):
-        for j in range(len(matrix[0])):
-            print(matrix[i][j], end=' ')
-        print()
-    print()
-
-
-def default_multiplication(a, b):
-    ar, ac, br, bc = len(a), len(a[0]), len(b), len(b[0])
-    p = Pool()
-    s = 0
-    t = []
-    res = []
-    for z in range(0, ar):
-        for j in range(0, bc):
-            for i in range(0, ac):
-                s = calculate_element((i, j, z), a, b, s)
-            t.append(s)
-            s = 0
-        res.append(t)
-        t = []
+def element(index):
+    global A
+    global B
+    i, j = index
+    res = 0
+    N = len(A[0]) or len(B)
+    for k in range(N):
+        res += A[i][k] * B[k][j]
     return res
 
+def procLen(m1,m2) -> int:
+    x = len(m1[0])
+    y = len(m2)
+    return x*y 
+def matrix1() -> list:
+    result = []
+    with open("matrix1","r") as f:
+        for i in f:
+            result.append(list(map(int,i.split())))
+    return result
 
-def calculate_element(index, a, b, s):
-    i, j, z = index
-    s += a[z][i] * b[i][j]
-    return s
+def matrix2() -> list:
+    result = []
+    with open("matrix2","r") as f:
+        for i in f:
+            result.append(list(map(int,i.split())))
+    return result
 
+def writeToFile(result):
+    open("result_matrix","w").close()
+    with open("result_matrix","a") as f:
+        for line in result:
+            print(" ".join(str(x) for x in line), file=f)
+        
+        
+A = matrix1()
+B = matrix2()
+print(A)
+print(B)
+pool = Pool(processes=procLen(A,B))
 
-def process_multiplication(x,y):
-    rez = sum(i*k for i, k in zip(x, y))
-    return rez
+k = pool.map(element, [(i,j) for i in range(len(A[0])) for j in range(len(B))])
+result = []
+for i in range(len(A[0])):
+    result.append([])
+    for j in range(len(B)):
+        result[-1].append(k[0])
+        k.remove(k[0])
 
-
-def reformat_matrix(matrix):
-    outsize = int(len(matrix)**(1/2))
-    outmatrix = []
-    row = []
-    for i, element in enumerate(matrix):
-        row.append(element)
-        if (i+1) % outsize == 0:
-            outmatrix.append(row)
-            row = []
-    return outmatrix
-
-
-def main():
-    matrix_size = int(input("Введите размер матрицы: "))
-    max_value = int(input("Введите максимальное значение элемента в матрицу: "))
-    matrix_a, matrix_b = matrix_gen(matrix_size, max_value)
-    print('Matrix A:')
-    print_matrix(matrix_a)
-    print('Matrix B:')
-    print_matrix(matrix_b)
-    with Pool(4) as pool:
-        matrix = pool.starmap(process_multiplication, [(i, k)for i in matrix_a for k in matrix_b])
-    print('Result:')
-    print_matrix(reformat_matrix(matrix))
-
-
-if __name__ == '__main__':
-    main()
+writeToFile(result)
