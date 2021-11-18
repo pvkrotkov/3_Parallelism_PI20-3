26689a6a3f8432c3c1a45f414e72858a8421709e
oskiiiiii <katya.khramtsova74@gmail.com>
2021-11-18 16:04:17 +0300

    new version

--- main.py
+++ main.py
@@ -1,66 +1,82 @@
-import random
-import time
-from multiprocessing import Process, Pool, Event
-import keyboard
+from multiprocessing import Process, Pool
+import csv
+from random import randint
 
 
-# определение элемента
-def element(index, A, B, size):
+def display(matrix):
+    max_len = 0
+    matrix = [x for x in matrix if x]
+    for row in matrix:
+        len_list = list(map(str, row))
+        max_len_local = len(max(len_list, key=len))
+        if max_len_local > max_len:
+            max_len = max_len_local
+    print('-'*((max_len+1)*len(matrix[0])+1))
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            print('|', end='')
+            print(str(matrix[i][j]).center(max_len), end='')
+        print('|')
+        print('-'*((max_len+1)*len(matrix[0])+1))
+
+
+def read_matrix(path):
+    with open(path, 'r') as matrix_csv:
+        matrix = list(csv.reader(matrix_csv, delimiter=';'))
+        matrix_list = list(map(lambda row: list(map(float, row)), matrix))
+    return matrix_list
+
+
+def element(element):
+    index = element[0]
+    A = element[1]
+    B = element[2]
     i, j = index
     res = 0
-    for k in range(size):
-        res += A[i][k] * B[k][j]
+    N = len(A[0]) or len(B)
+    for k in range(N):
+        num = round(A[i][k] * B[k][j], 2)
+        res += num
     return res
 
 
-# установка размера матрицы
-def set_size(size):
-    return [[random.randint(-100, 100) for j in range(size)] for i in range(size)]
+def gen_matrix(n, k):
+    matrix = [[randint(-100, 100) for _ in range(n)] for _ in range(n)]
 
+    with open("matrix_"+str(k)+".csv", 'w') as matrix_csv:
+        writer = csv.writer(matrix_csv, delimiter=';')
+        for row in matrix:
+            writer.writerow(row)
+    return matrix
 
-# запись матрицы c (почти) уникальным ID
-def write_out(mat, name):
-    for idents, item in enumerate(mat):
-        with open(f'{name[0]}_{random.randint(1, 1000)}.txt', 'w') as file:
-            file.write(f"Matrix {idents}:\n")
-            for i in item:
-                file.write(' '.join([str(j) for j in i]) + '\n')
-
-
-# получение результата
-def get_result(arr, size, pool):
-    for i in range(size):
-        for j in range(size):
-            result = pool.apply_async(element, ((i, j), arr[0], arr[1], size))
-            res_matrix = result.get()
-            arr[2][i].append(res_matrix)
-    return arr[2]
-
-
-# основное вычисление c автоматическим выборо кол-ва процессов
-def general_operations(size, ev_stop):
-    while ev_stop.is_set():
-        with Pool(processes=size * 2) as pool:
-            array1, array2 = set_size(size), set_size(size)
-            write_out([array1, array2], ['matrix_1', 'matrix_2'])
-            array3 = [[] for i in range(size)]
-            array3 = get_result([array1, array2, array3], size, pool)
-            write_out([array3], ['matrix_3'])
-            time.sleep(5)
-
-
-# основная функция с остановкой процесса и заданием размера матрицы
 def main():
-    ev = Event()
-    ev.set()
-    size = int(input("Задайте размер матрицам: "))
-    main_event = Process(target=general_operations, args=(size, ev))
-    main_event.start()
-    while keyboard.read_key() == "p":
-        print("Exiting from program")
-        break
-    ev.clear()
-    main_event.join()
+    while True:
+        try:
+            user_input = int(input("Введите 0, чтобы выйти.\nВведите размер матриц: "))
+            if (user_input == 0):
+                break
+        except ValueError:
+            print("Введите число")
+            continue
+        else:
+            matrix_1 = gen_matrix(user_input, 1)
+            matrix_2 = gen_matrix(user_input, 2)
+            num_of_processes = Pool(processes=len(matrix_1)*len(matrix_2[0]))
+            elements = [[[i, j], matrix_1, matrix_2] for i in range(len(matrix_1)) for j in range(len(matrix_2[0]))]
+            result = num_of_processes.map(element, elements)
+
+            with open('result.csv', 'w') as res:
+                writer = csv.writer(res, delimiter=';')
+                for row in range(0, len(result), len(matrix_2[0])):
+                    writer.writerow(result[row:row + len(matrix_2[0])])
+
+            matrix = read_matrix("result.csv")
+            print('Матрица 1')
+            display(matrix_1)
+            print('Матрица 2')
+            display(matrix_2)
+            print('Результат')
+            display(matrix)
 
 
 if __name__ == '__main__':
